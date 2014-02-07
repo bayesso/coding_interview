55f37504ac086334bb14b319587e34bb2bb2ae0c
bayesso <bayesso@gmail.com>
2014-02-06 16:09:25 -0800

    Update Solution.java

--- leetcode/SingleNumber/Solution.java
+++ leetcode/SingleNumber/Solution.java
@@ -3,7 +3,7 @@
  * Given an array of integers, every element appears twice except for one. Find that single one. 
  * Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
  * [Solution idea]: 
- * Use the property of XORin bit manipulation: x^x=0 and x^1=x
+ * Use the property of XORin bit manipulation: x^x=0 and x^0=x
  * /
 
 public class Solution {
